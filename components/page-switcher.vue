e30cdbef7ce2ec41f37b930d9c8348e937cfbcbb
laosb <laosb@lasteam.net>
2017-07-11 23:16:24 +0800

    Conceptually working page switcher.

---
+++ components/page-switcher.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="uk-pagination" uk-margin>
+    <li v-if="currentPage > 1"><a :href="'#page-' + (currentPage - 1)"><span uk-pagination-previous></span></a></li>
+    <li ><a href="#">1</a></li>
+    <li v-if="currentPage > 4" class="uk-disabled"><span>...</span></li>
+    <li v-if="currentPage > 3"><a :href="'#page-' + (currentPage - 3)">{{currentPage - 3}}</a></li>
+    <li v-if="currentPage > 2"><a :href="'#page-' + (currentPage - 2)">{{currentPage - 2}}</a></li>
+    <li v-if="currentPage > 1"><a :href="'#page-' + (currentPage - 1)">{{currentPage - 1}}</a></li>
+    <li class="uk-active"><span>{{currentPage}}</span></li>
+    <li v-if="currentPage < pageCount"><a :href="'#page-' + (currentPage + 1)">{{currentPage + 1}}</a></li>
+    <li v-if="currentPage < (pageCount - 1)"><a :href="'#page-' + (currentPage + 2)">{{currentPage + 2}}</a></li>
+    <li v-if="currentPage < (pageCount - 2)"><a :href="'#page-' + (currentPage + 3)">{{currentPage + 3}}</a></li>
+    <li v-if="currentPage < (pageCount - 3)" class="uk-disabled"><span>...</span></li>
+    <li><a :href="'#page-' + pageCount">{{pageCount}}</a></li>
+    <li v-if="currentPage < pageCount"><a :href="'#page-' + (currentPage + 1)"><span uk-pagination-next></span></a></li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    itemCount: Number,
+  },
+  computed: {
+    pageCount() { return Math.ceil(this.itemCount / 20) },
+    currentPage() {
+      return this.$route.hash && this.$route.hash.startsWith('page-') ?
+        parseInt(this.$route.hash.split('-')[1], 10) :
+        1 // Defaults to 1.
+    }
+  }
+}
+</script>
