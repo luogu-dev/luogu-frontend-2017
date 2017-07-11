90212fd3caa0299d42a71c854038052a9ae8b747
laosb <laosb@lasteam.net>
2017-07-11 23:56:36 +0800

    constants about contest

---
+++ assets/js/contest-consts.js
@@ -0,0 +1,21 @@
+export const contestMethods = [
+  { /* 0, not in use */ },
+  { name: 'OI', color: 'red' }, // 1
+  { name: 'ACM', color: 'red' }, // 2
+  { name: '乐多', color: 'red' }, // 3
+  { name: 'IOI', color: 'red' }, // 4
+  { name: 'CF', color: 'red' }, // 5
+  { name: '夺时', color: 'red' } // 6
+]
+
+export const contestPublic = [
+  { /* 0, not in use */ },
+  { name: '官方比赛', color: 'red' }, // 1
+  { name: '团队公开赛', color: 'red' }, // 2
+  { name: '团队内部赛', color: 'red' }, // 3
+  { name: '个人公开赛', color: 'red' }, // 4
+  { name: '个人邀请赛', color: 'red' }, // 5
+  { name: '团队邀请赛', color: 'red' }, // 6
+  { name: '团队公开赛 (待审核)', color: 'red' }, // 7
+  { name: '个人公开赛 (待审核)', color: 'red' }, // 8
+]
