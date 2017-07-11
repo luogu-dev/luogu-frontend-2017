8830f56641c2472bc50cd1c34c7795995aff7683
laosb <laosb@lasteam.net>
2017-07-12 00:58:38 +0800

    simple list of contests

---
+++ components/contest-item.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <a href="#" :class="isEnded ? 'uk-link-muted' : ''">
+      {{contest.Name}}
+      <span class="uk-align-right lg-no-margin-bottom">
+        <span :class="'uk-label uk-margin-small-right lg-bg-color-' + methodLabel.color">{{methodLabel.name}}</span>
+        <span :class="'uk-label uk-margin-small-right lg-bg-color-' + publicLabel.color">{{publicLabel.name}}</span>
+      </span>
+    </a>
+  </li>
+</template>
+<style lang="less" scoped>
+.lg-no-margin-bottom { margin-bottom: 0 }
+</style>
+<script>
+import { contestMethods, contestPublic } from '~assets/js/contest-consts'
+
+export default {
+  props: {
+    contest: Object
+  },
+  computed: {
+    publicLabel() { return contestPublic[this.contest.Public] },
+    methodLabel() { return contestMethods[this.contest.Method] },
+    isEnded() { return (new Date()) > (new Date(this.contest.EndTime * 1000)) }
+  }
+}
+</script>
---
+++ pages/contests/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="lg-float-title">比赛</h1>
+    <div class="uk-card uk-card-default uk-card-body uk-margin">
+      <ul class="uk-list uk-list-divider">
+        <contest-item v-for="contest in contests.result" :key="contest.TID" :contest="contest"/>
+      </ul>
+      <page-switcher :itemCount="contests.count"/>
+    </div>
+  </div>
+</template>
+<script>
+import PageSwitcher from '~components/page-switcher'
+import ContestItem from '~components/contest-item'
+
+import { get } from '~plugins/lgapi'
+
+export default {
+  async asyncData(){
+    return { contests: await get(`/api/contest/lists`) }
+  },
+  components: { PageSwitcher, ContestItem }
+}
+</script>
