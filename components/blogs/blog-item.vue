4ffb8e89eb80ffda52e063d4eccb65a56a099c88
laosb <laosb@lasteam.net>
2017-07-14 02:59:56 +0800

    Use user-link.

--- components/blogs/blog-item.vue
+++ components/blogs/blog-item.vue
@@ -1,17 +1,18 @@
 <template>
   <li>
     <router-link :to="{ name: 'blogs-id', params: { id: blog.BlogID } }">
-      {{blog.Title}}
-      <span class="uk-align-right lg-no-margin-bottom uk-text-meta">
-        by {{blog.Author.Username}} @ {{blog.PostTime | readableDate}}
-      </span>
+      <span>{{blog.Title}}</span>
     </router-link>
+    <span class="uk-align-right lg-no-margin-bottom uk-text-meta"><user-link :user="blog.Author"/> @ {{blog.PostTime | readableDate}}</span>
   </li>
 </template>
 <script>
+import UserLink from '~components/common/user-link'
+
 export default {
   props: {
     blog: Object
-  }
+  },
+  components: { UserLink }
 }
 </script>
--- components/common/user-link.vue
+++ components/common/user-link.vue
@@ -1,8 +1,8 @@
 <template>
   <span>
-    <router-link :class="'uk-link-reset lg-color-' + user.level_color" :to="{ name: 'users-id', params: { id: user.UID } }">{{user.Username}}</router-link>
-    <span v-if="user.isAdmin" class="uk-label uk-margin-small-left">管理员</span>
-    <span v-if="!user.isValid" class="uk-label uk-margin-small-left">已封禁</span>
+    <router-link :class="'uk-link-reset lg-color-' /*+ user.level_color*/" :to="{ name: 'users-id', params: { id: user.UID } }">{{user.Username}}</router-link>
+    <!-- <span v-if="user.isAdmin" class="uk-label uk-margin-small-left">管理员</span>
+    <span v-if="!user.isValid" class="uk-label uk-margin-small-left">已封禁</span> -->
   </span>
 </template>
 <script>
