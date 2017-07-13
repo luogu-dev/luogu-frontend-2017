4273bc0385bc9f6d177d81e53b15fe53232afb0d
laosb <laosb@lasteam.net>
2017-07-14 02:59:41 +0800

    Update to use moment.

--- plugins/global-filters.js
+++ plugins/global-filters.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
+import moment from '~plugins/moment'
 
-Vue.filter('readableDate', date => (new Date(date * 1000)).toLocaleString())
+Vue.filter('readableDate', timestamp => moment(timestamp * 1000).calendar())
